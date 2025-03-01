7e686c45bc880629f00ee816c032f599e4ca11c0
sunita <160343306+suma01111@users.noreply.github.com>
2025-03-01 19:21:05 +0530

    Time: 0 ms (100%), Space: 13.1 MB (87.59%) - LeetHub

---
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> applyOperations(vector<int>& nums) {
+        int n = nums.size();
+        for(int i=0;i<n-1;i++){
+            if (nums[i]==nums[i+1]){
+                nums[i] *= 2;
+                nums[i+1] = 0;
+            }
+        }
+
+        int non_zero_idx = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != 0) {
+                swap(nums[i], nums[non_zero_idx]);
+                non_zero_idx++;
+            }
+        }
+
+        return nums;
+    }
+};
