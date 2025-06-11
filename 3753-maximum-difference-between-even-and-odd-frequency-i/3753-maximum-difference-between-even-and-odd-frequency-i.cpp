efbdffe61fac57f13865114366ce1c21f6317aac
sunita <160343306+suma01111@users.noreply.github.com>
2025-06-11 10:21:05 +0530

    Time: 2 ms (43.74%), Space: 9.2 MB (94.34%) - LeetHub

--- 3753-maximum-difference-between-even-and-odd-frequency-i/3753-maximum-difference-between-even-and-odd-frequency-i.cpp
+++ 3753-maximum-difference-between-even-and-odd-frequency-i/3753-maximum-difference-between-even-and-odd-frequency-i.cpp
@@ -1,18 +1,41 @@
 class Solution {
 public:
     int maxDifference(string s) {
-        unordered_map<char, int> c;
-        for (char ch : s) {
-            ++c[ch];
+        int n = s.length();
+
+        int maxOdd  = 0;
+        int minEven = n+1;
+
+        vector<int> vec(26, 0); //IMPORTANT
+        for(char &ch : s) {
+            vec[ch-'a']++; //IMPORTANT
         }
-        int maxOdd = 1, minEven = s.size();
-        for (const auto& [_, value] : c) {
-            if (value % 2 == 1) {
-                maxOdd = max(maxOdd, value);
+
+        for(int i = 0; i < 26; i++) {
+            if(vec[i] == 0)
+                continue;
+
+            if(vec[i]%2 == 0) {
+                minEven = min(minEven, vec[i]);
             } else {
-                minEven = min(minEven, value);
+                maxOdd = max(maxOdd, vec[i]);
             }
         }
+
         return maxOdd - minEven;
     }
 };
+
+/*
+English lowercase letters range from 'a' to 'z', i.e., 26 letters total.
+This line creates a vector vec of size 26, all initialized to 0.
+Each index in the vector corresponds to a letter:
+vec[0] → frequency of 'a'
+vec[1] → frequency of 'b'
+...
+vec[25] → frequency of 'z'
+
+ch - 'a' calculates the index for that letter:
+For example, if ch = 'c', then 'c' - 'a' = 2 → it increments vec[2] by 1.
+
+*/
