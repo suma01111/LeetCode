8beb7e6d18a9c104268d2e28c3533095a0653bb3
sunita <160343306+suma01111@users.noreply.github.com>
2025-06-21 12:11:36 +0530

    Time: 2782 ms (5.07%), Space: 34.3 MB (87.28%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,21 +1,14 @@
-// striver
-
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        unordered_map<int,int> pSum_cnt;
-        pSum_cnt[0] = 1; //prefixSum=0 and cnt = 1;
-
-        int prefixSum = 0;
         int cnt =0;
 
-
         for(int i=0;i<nums.size();i++){
-            prefixSum+= nums[i];
-
-            int remove = prefixSum - k ;
-            cnt+= pSum_cnt[remove]; //if remove is available then cnt ++
-            pSum_cnt[prefixSum] +=1;//put prefixSum in map with cnt++wrt that sum 
+            int sum =0;
+            for(int j=i;j<nums.size();j++){
+                sum+= nums[j];
+                if(sum==k) cnt++;
+            }
         }
         return cnt;
     }
