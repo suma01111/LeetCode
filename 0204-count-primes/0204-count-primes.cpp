44861cf03f028a01838e2f5ee56a002f2bf6d265
sunita <160343306+suma01111@users.noreply.github.com>
2025-09-05 15:14:24 +0530

    Time: 179 ms (62.35%), Space: 12.7 MB (66.52%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -1,18 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
     int countPrimes(int n) {
-        if (n<=1) return 0;
+        if (n <= 2) return 0; // no primes less than 2
+        
+        vector<bool> isPrime(n, true); //all are prime initially
+        isPrime[0] = isPrime[1] = false;
 
-        vector<int> isPrime(n,1); //all true
-        isPrime[0] = isPrime[1] = 0; //false 
-
-        for(int i=2 ; i*i<n;i++){
-            if(isPrime[i]){
-                for(int j=i*i; j<n; j+=i){
-                    isPrime[j] = 0;
+        for (int i = 2; i * i < n; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j < n; j += i) {
+                    isPrime[j] = false;
                 }
             }
         }
-        return count(isPrime.begin(), isPrime.end(), 1);
+
+        return count(isPrime.begin(), isPrime.end(), true);
     }
 };
