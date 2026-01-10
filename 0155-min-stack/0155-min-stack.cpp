5916b64ba39c0a5d44e304ecd81944cf13b87791
sunita <160343306+suma01111@users.noreply.github.com>
2026-01-10 23:14:32 +0530

    Time: 5 ms (28.64%), Space: 23.8 MB (17.95%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,38 +1,48 @@
+//2nd submission in 0(N) , 1st 0(2N) is still easy , 
 class MinStack {
-public:
-//pair values in stack (first,second) = (val,minVal)
-stack<pair<int,int>> minSt;
+    stack<long long> s;
+    long long minVal;
 
+public:
     MinStack() {
-        
+        // Constructor to initialize stack and minVal
+        while (!s.empty()) s.pop();
     }
-    
+
     void push(int val) {
-        if(minSt.empty()) minSt.push({val,val});
-        else {
-            int minVal = min(val,minSt.top().second);
-            minSt.push({val,minVal});
+        if (s.empty()) {
+            s.push(val);
+            minVal = val;
+        } else {
+            if (val < minVal) {
+                // Encode the new min value
+                s.push(2LL * val - minVal);
+                minVal = val;
+            } else {
+                s.push(val);
             }
+        }
     }
-    
+
     void pop() {
-        minSt.pop();
+        if (s.empty()) return;
+
+        if (s.top() < minVal) {
+            // Restore previous minVal
+            minVal = 2LL * minVal - s.top();
+        }
+        s.pop();
     }
-    
+
     int top() {
-        return minSt.top().first;
+        if (s.top() < minVal) {
+            // Top is encoded
+            return minVal;
+        }
+        return s.top();
     }
-    
+
     int getMin() {
-        return minSt.top().second;
+        return minVal;
     }
 };
-
-/**
- * Your MinStack object will be instantiated and called as such:
- * MinStack* obj = new MinStack();
- * obj->push(val);
- * obj->pop();
- * int param_3 = obj->top();
- * int param_4 = obj->getMin();
- */
