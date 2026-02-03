a9f09d86ccda37240188f84049fb855040674590
sunita <160343306+suma01111@users.noreply.github.com>
2026-02-03 22:43:58 +0530

    Time: 0 ms (100%), Space: 16.3 MB (84.46%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
@@ -9,28 +9,27 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
- //brute force O(n^2), simply compare to root  and insert 
 class Solution {
 public:
-    TreeNode* helper(vector<int>& preorder, int start, int end){
-        if(start>end) return NULL;
-        TreeNode* root = new TreeNode(preorder[start]);
+    int idx = 0;
 
-        // Find first element greater than root, IMPPP
-        int idx = start+1;
-        while (idx <= end && preorder[idx] < root->val)
-            idx++;
+    TreeNode* build(vector<int>& preorder, int bound) {
+        
+        if (idx == preorder.size() || preorder[idx] > bound) //boundary
+            return NULL;
 
-        root->left = helper(preorder,start+1, idx-1); //Imp
-        root->right = helper(preorder,idx, end); //IMP
+        TreeNode* root = new TreeNode(preorder[idx++]);
+
+        
+        root->left = build(preorder, root->val); // Left subtree: values < root->val
+
+        
+        root->right = build(preorder, bound); // Right subtree: values < bound but > root->val
 
         return root;
-
     }
+
     TreeNode* bstFromPreorder(vector<int>& preorder) {
-        return helper(preorder, 0, preorder.size()-1);
+        return build(preorder, INT_MAX);
     }
 };
-
-
