a0bc0beba9ced067b76e7f79374fb51722ac94ad
sunita <160343306+suma01111@users.noreply.github.com>
2026-02-15 00:22:45 +0530

    Time: 0 ms (100%), Space: 11.4 MB (67.43%) - LeetHub

---
+++ 1389-create-target-array-in-the-given-order/1389-create-target-array-in-the-given-order.cpp
@@ -0,0 +1,14 @@
+//automatically shift everything on the right
+//ans.insert(ans.begin() + pos, value);
+
+class Solution {
+public:
+    vector<int> createTargetArray(vector<int>& nums, vector<int>& index) {
+        vector<int> ans;
+
+        for(int i=0;i<nums.size();i++){
+            ans.insert(ans.begin()+index[i], nums[i]);
+        }
+        return ans;
+    }
+};
