905591d5f19c024cffdd8cb83a26105538284aec
sunita <160343306+suma01111@users.noreply.github.com>
2026-02-15 11:41:21 +0530

    Time: 4 ms (15.96%), Space: 13.7 MB (43.76%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
@@ -1,10 +1,16 @@
 class Solution {
 public:
     bool checkIfExist(vector<int>& arr) {
-        for(int i=0;i<arr.size();i++){
-            for(int j=0;j<arr.size();j++){
-                if (i != j && arr[i] == 2 * arr[j]) return true;
-            }
+        unordered_set<int> s;
+
+        for (int x : arr) {
+            if (s.find(2 * x) != s.end()) 
+                return true;
+
+            if (x % 2 == 0 && s.find(x / 2) != s.end()) 
+                return true;
+
+            s.insert(x);
         }
         return false;
     }
