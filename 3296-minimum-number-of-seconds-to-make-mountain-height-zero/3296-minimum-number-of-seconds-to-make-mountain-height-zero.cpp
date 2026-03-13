5f2a3c41b3c411c460199c054422fab5a3455c46
sunita <160343306+suma01111@users.noreply.github.com>
2026-03-13 19:04:14 +0530

    Time: 5 ms (89.68%), Space: 28.9 MB (90.32%) - LeetHub

---
+++ 3296-minimum-number-of-seconds-to-make-mountain-height-zero/3296-minimum-number-of-seconds-to-make-mountain-height-zero.cpp
@@ -0,0 +1,40 @@
+//Binary search
+class Solution {
+public:
+    long long minNumberOfSeconds(int mountainHeight, vector<int>& workerTimes) {
+        long long l =1; //lowest time
+
+        // int t_max = 0;
+        // for(int i=0;i<workerTimes.size();i++){
+        //     t_max = max(t_max,workerTimes[i]);
+        // }
+        int t_max = *max_element(workerTimes.begin(), workerTimes.end());
+
+
+        long long h = 1LL * t_max * mountainHeight * (mountainHeight + 1) / 2;  //highest time 
+        long long result = h;
+
+        while(l<=h){
+            long long mid = l+(h-l)/2 ;  //mid time
+            if(check(mid,mountainHeight, workerTimes )){ //recursion call;
+                result = mid;
+                h=mid-1;
+            }
+            else l=mid+1;
+        }
+        return result;
+    }
+
+    bool check(long long midTime, int mountainHeight, vector<int>& workerTimes){
+        long long totalHeight = 0; //height
+        for(int t : workerTimes){
+            //sum of all reducinh height of worker at midTime
+            totalHeight += (long long)(sqrt((2.0 * midTime) / t + 0.25) - 0.5);
+ 
+
+            if(totalHeight>=mountainHeight) return true; 
+        }
+        return  false;//h>=mountainHeight
+    }
+    
+};
