7af88605a5ab11ac674c1b4f40d6a4fd8cb1207a
sunita <160343306+suma01111@users.noreply.github.com>
2026-03-15 11:45:12 +0530

    Time: 0 ms (100%), Space: 26.3 MB (34.37%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -2,33 +2,22 @@ class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
         
-        
-        int high = nums.size()-1;
         int low = 0;
-        int mid;
-        
-        //Boundary cases, what if we get unique elements at boundary lets check
-        if(high==0)
-            return nums[0];
-        else if(nums[0]!=nums[1])
-            return nums[0];
-        else if(nums[high]!=nums[high-1])
-            return nums[high];
+        int high = nums.size() - 1;
 
-        while(low<=high)
+        while(low < high)
         {
-            mid = low + (high-low)/2;
-            //Unique element condition
-            if(nums[mid]!=nums[mid+1] && nums[mid]!=nums[mid-1])
-                return nums[mid];
-            
-            if(((mid%2)==0 && nums[mid]==nums[mid+1])
-               ||  ((mid%2)==1 && nums[mid]==nums[mid-1]))
-                low = mid+1;
+            int mid = (low + high) / 2;
+
+            if(mid % 2 == 1)
+                mid--;   // make mid even
+
+            if(nums[mid] == nums[mid+1])
+                low = mid + 2;
             else
-                high = mid-1;
-            
+                high = mid;
         }
-        return -1;
+
+        return nums[low];
     }
 };
