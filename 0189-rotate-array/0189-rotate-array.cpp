2d20aea343d68e8b222a8b9887b1dbcfacb50a19
sunita <160343306+suma01111@users.noreply.github.com>
2026-03-21 22:32:10 +0530

    Time: 0 ms (100%), Space: 29.6 MB (42.57%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,22 +1,17 @@
-//optimal sol , sub 2
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
+        k=k%n; //handle cases k>=n
 
-        k = k % n; // Handle cases where k > n
-
-
-        reverse(nums, 0, n - 1);
-
-        reverse(nums, 0, k - 1);
- 
-        reverse(nums, k, n - 1);
+        reverse(nums, 0,n-k-1);
+        reverse(nums, n-k,n-1);
+        reverse(nums, 0,n-1);
     }
 
 private:
-    void reverse(vector<int>& nums, int start, int end) {
-        while (start < end) {
+    void reverse(vector<int>& nums, int start, int end){
+        while(start<end){
             int temp = nums[start];
             nums[start] = nums[end];
             nums[end] = temp;
