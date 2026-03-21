5de5a2ad02a1e3c80fbd0c0c83eb3173e09f89ba
sunita <160343306+suma01111@users.noreply.github.com>
2026-03-22 00:21:50 +0530

    Time: 0 ms (100%), Space: 34 MB (31.02%) - LeetHub

--- 3643-flip-square-submatrix-vertically/3643-flip-square-submatrix-vertically.cpp
+++ 3643-flip-square-submatrix-vertically/3643-flip-square-submatrix-vertically.cpp
@@ -1,20 +1,16 @@
+//optimal 
 class Solution {
 public:
     vector<vector<int>> reverseSubmatrix(vector<vector<int>>& grid, int x, int y, int k) {
-        stack<int> st;
-        int m = grid.size();
-        int n = grid[0].size();
-
-        for(int j=y;j<y+k;j++){
-            for(int i=x;i<x+k; i++){
-                st.push(grid[i][j]);
-            }
+        
+        for(int j = y; j < y + k; j++) {
+            int top = x;
+            int bottom = x + k - 1;
             
-            int z=x; 
-            while(!st.empty()){
-                grid[z][j]=st.top();
-                z++;
-                st.pop();
+            while(top < bottom) {
+                swap(grid[top][j], grid[bottom][j]);
+                top++;
+                bottom--;
             }
         }
 
