dad94cb03622535b6791bd7be474206d785b13f5
sunita <160343306+suma01111@users.noreply.github.com>
2026-03-22 00:22:22 +0530

    Time: 3 ms (28.92%), Space: 34.6 MB (5.12%) - LeetHub

--- 3643-flip-square-submatrix-vertically/3643-flip-square-submatrix-vertically.cpp
+++ 3643-flip-square-submatrix-vertically/3643-flip-square-submatrix-vertically.cpp
@@ -1,16 +1,23 @@
-//optimal 
+// by me , i used SC (O(N))
+
+
 class Solution {
 public:
     vector<vector<int>> reverseSubmatrix(vector<vector<int>>& grid, int x, int y, int k) {
-        
-        for(int j = y; j < y + k; j++) {
-            int top = x;
-            int bottom = x + k - 1;
+        stack<int> st;
+        int m = grid.size();
+        int n = grid[0].size();
+
+        for(int j=y;j<y+k;j++){
+            for(int i=x;i<x+k; i++){
+                st.push(grid[i][j]);
+            }
             
-            while(top < bottom) {
-                swap(grid[top][j], grid[bottom][j]);
-                top++;
-                bottom--;
+            int z=x; 
+            while(!st.empty()){
+                grid[z][j]=st.top();
+                z++;
+                st.pop();
             }
         }
 
