ef827e9098303e79b4b424f5ec06cd01218af975
sunita <160343306+suma01111@users.noreply.github.com>
2026-03-22 15:48:35 +0530

    Time: 0 ms (100%), Space: 17.1 MB (41.14%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,69 +1,61 @@
+// striver sol
+
+
 class Solution {
 public:
-//Multi-Source BFS
-
     int orangesRotting(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
         
-        queue<pair<pair<int,int>, int>> Q;// queue stores: {{i, j}, time}
-        vector<vector<int>> visited(m, vector<int>(n, 0));//m*n mat of 0 or false
-
-        //push rotton oranges in queue first(as a root of bfs)
+        vector<vector<int>> vis(m, vector<int>(n,0));
+        queue<pair<pair<int,int>,int>> q;
+        
+        // push all rotten oranges
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]==2){
-                Q.push({{i,j},0});
-                visited[i][j]=1;
+                    vis[i][j]=1;
+                    q.push({{i,j},0});
                 }
             }
         }
-
-        int ans=0;
-        //bfs
-        while(!Q.empty()){
-            auto node = Q.front();
-            Q.pop();
-
-            int i= node.first.first;
+        
+        int ans = 0;
+        
+        int drow[] = {-1, 1, 0, 0};
+        int dcol[] = {0, 0, -1, 1};
+        
+        while(!q.empty()){
+            auto node = q.front();
+            q.pop();
+            
+            int i = node.first.first;
             int j = node.first.second;
             int time = node.second;
-
+            
             ans = max(ans, time);
             
-            // up
-            if(i-1 >= 0 && !visited[i-1][j] && grid[i-1][j] == 1){
-                Q.push({{i-1, j}, time + 1});
-                visited[i-1][j] = 1;
+            for(int k=0;k<4;k++){
+                int ni = i + drow[k];
+                int nj = j + dcol[k];
+                
+                if(ni>=0 && ni<m && nj>=0 && nj<n &&
+                   !vis[ni][nj] && grid[ni][nj]==1){
+                    
+                    vis[ni][nj] = 1;
+                    q.push({{ni,nj}, time+1});
+                }
             }
-
-            // left
-            if(j-1 >= 0 && !visited[i][j-1] && grid[i][j-1] == 1){
-                Q.push({{i, j-1}, time + 1});
-                visited[i][j-1] = 1;
-            }
-
-            // down
-            if(i+1 < m && !visited[i+1][j] && grid[i+1][j] == 1){
-                Q.push({{i+1, j}, time + 1});
-                visited[i+1][j] = 1;
-            }
-
-            // right
-            if(j+1 < n && !visited[i][j+1] && grid[i][j+1] == 1){
-                Q.push({{i, j+1}, time + 1});
-                visited[i][j+1] = 1;
-            }
-
         }
-
-        //check if any fresh left
+        
+        // check any fresh left
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(grid[i][j]==1 && !visited[i][j]) return -1;
+                if(grid[i][j]==1 && !vis[i][j])
+                    return -1;
             }
         }
-
-        return ans ;
+        
+        return ans;
     }
 };
