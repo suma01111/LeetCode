9fb62ab31496ae9f71430006d9093ad69e48df20
sunita <160343306+suma01111@users.noreply.github.com>
2026-03-22 14:09:24 +0530

    Time: 0 ms (100%), Space: 14.5 MB (70.77%) - LeetHub

--- 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.cpp
+++ 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.cpp
@@ -2,27 +2,30 @@
 
 class Solution {
 public:
-    bool findRotation(vector<vector<int>>& mat, vector<vector<int>>& target) {
+
+    void rotate90(vector<vector<int>>& mat) {
         int n = mat.size();
 
-        for(int k = 0; k < 4; k++) {
-            if(mat == target) return true;
-
-            // rotate 90 degree
-
-            // transpose
-            for(int i = 0; i < n; i++) {
-                for(int j = i + 1; j < n; j++) {
-                    swap(mat[i][j], mat[j][i]);
-                }
-            }
-
-            // reverse each row
-            for(int i = 0; i < n; i++) {
-                reverse(mat[i].begin(), mat[i].end());
+        // transpose
+        for(int i = 0; i < n; i++) {
+            for(int j = i + 1; j < n; j++) {
+                swap(mat[i][j], mat[j][i]);
             }
         }
+        // reverse each row
+        for(int i = 0; i < n; i++) {
+            reverse(mat[i].begin(), mat[i].end());
+        }
+    }
 
+
+    bool findRotation(vector<vector<int>>& mat, vector<vector<int>>& target) {
+// k < 4 ? becoz a sq mat has only 4 possible orientations:original,90°,180°,270°
+        for(int k = 0; k < 4; k++) {
+            if(mat == target) return true; 
+
+            rotate90(mat); // rotate for next check
+        }
         return false;
     }
-};
+};;
