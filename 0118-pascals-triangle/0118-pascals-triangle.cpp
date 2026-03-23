b894835dfa4680bd7f66bc6b74de8831a20f5c14
sunita <160343306+suma01111@users.noreply.github.com>
2026-03-23 22:57:35 +0530

    Time: 0 ms (100%), Space: 9.7 MB (30.23%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,16 +1,19 @@
+//Better approach O(n^2)=TC=SC
+
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>> result(numRows); 
+
         for(int i=0;i<numRows;i++){
             result[i].resize(i+1);
         }
-        result[0][0] = 1;
+        result[0][0] = 1; //row 0th
 
         for(int i=1;i<numRows;i++){
             for(int j=0;j<=i;j++){
-                if(j==0) result[i][j]=1;
-                else if(i==j) result[i][j]=1;
+                if(j==0) result[i][j]=1; //first element of every row start from (1th)
+                else if(i==j) result[i][j]=1; //last element 
                 else{
                     result[i][j]=result[i-1][j-1] + result[i-1][j];
                 }
