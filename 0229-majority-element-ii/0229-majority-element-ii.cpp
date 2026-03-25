3780d2003db617f86137d539eb2895dfecd8b84d
sunita <160343306+suma01111@users.noreply.github.com>
2026-03-26 02:28:19 +0530

    Time: 0 ms (100%), Space: 23.6 MB (82.99%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,20 +1,46 @@
+// ✅ Optimal Approach (Moore’s Voting Extended)
+// We can have at most 2 elements appearing more than ⌊n/3⌋ times
+// So we maintain:
+// candidate1, candidate2
+// count1, count2
+
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        unordered_map<int,int> mp;
-        vector<int> ans;
-
-        int n = nums.size();
+        int cnt1=0, cnt2=0;
+        int ele1 = INT_MIN; //candidate 1
+        int ele2 = INT_MIN; //candidate 2
 
         for(int i=0;i<nums.size();i++){
-            mp[nums[i]]++;
-        }
 
-        for(auto it : mp){
-            if(it.second > n/3){
-                ans.push_back(it.first);
+            if(cnt1 ==0 && nums[i]!= ele2){
+                cnt1++;
+                ele1 = nums[i];
+            }
+
+            else if(cnt2 ==0 && nums[i]!= ele1){
+                cnt2++;
+                ele2 = nums[i];
+            }
+
+            else if(nums[i]==ele1) cnt1++;
+            else if(nums[i]==ele2) cnt2++;
+
+            else {
+                cnt1--; cnt2--;
             }
         }
+
+        // verification step
+        cnt1 = cnt2 = 0;
+        for(int num : nums){
+            if(num == ele1) cnt1++;
+            else if(num == ele2) cnt2++;
+        }
+
+        vector<int> ans;
+        if(cnt1 > nums.size()/3) ans.push_back(ele1);
+        if(cnt2 > nums.size()/3) ans.push_back(ele2);
 
         return ans;
     }
