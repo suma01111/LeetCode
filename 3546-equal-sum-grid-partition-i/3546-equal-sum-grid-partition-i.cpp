47648c2a686249dbadeae36e448562f7c05a5cb0
sunita <160343306+suma01111@users.noreply.github.com>
2026-03-25 16:48:58 +0530

    Time: 15 ms (35.61%), Space: 130.1 MB (53.24%) - LeetHub

--- 3546-equal-sum-grid-partition-i/3546-equal-sum-grid-partition-i.cpp
+++ 3546-equal-sum-grid-partition-i/3546-equal-sum-grid-partition-i.cpp
@@ -1,12 +1,20 @@
+//optimal O(M*N) , SC=O(M+N);
+
 class Solution {
 public:
     bool canPartitionGrid(vector<vector<int>>& grid) {
         long long prefix=0;
+        
         int m = grid.size();
         int n = grid[0].size();
+        vector<long long> row(m,0);
+        vector<long long> col(n,0);
 
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
+                row[i] += grid[i][j]; //Understand this IMPP
+                col[j] += grid[i][j];//--||--, storing row and col sum in one n^2 loop
+
                 prefix += grid[i][j];
             }
         }
@@ -15,24 +23,18 @@ public:
 
         long long target = prefix/2;
 
-        //horizontal
-        long long Rsum=0;
+        //horizontal cut
+        long long Rsum =0;
         for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                // cout<<"x";
-                Rsum +=grid[i][j];
-            }
-            if(Rsum == target) return true;
+            Rsum += row[i];
+            if(Rsum==target) return true;
         }
 
-        //vertical
-        long long Csum=0;
+        //verticatal cut
+        long long Csum =0;
         for(int j=0;j<n;j++){
-            for(int i=0;i<m;i++){
-                // cout<<"y";
-                Csum +=grid[i][j];
-            }
-            if(Csum == target) return true;
+            Csum += col[j];
+            if(Csum==target) return true;
         }
 
         return false;
