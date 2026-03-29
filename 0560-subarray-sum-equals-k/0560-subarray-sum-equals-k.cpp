c36a387e846d4ebe028ae40f5288f4e4697cb363
sunita <160343306+suma01111@users.noreply.github.com>
2026-03-29 15:33:08 +0530

    Time: 33 ms (90.63%), Space: 45.4 MB (69.5%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,31 +1,22 @@
-//shradha bhabhi
-
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        int n = nums.size();
-        int cnt =0;
+        unordered_map<int,int> m;
+        m[0] = 1;  // important for subarrays starting at index 0
+        
+        int sum = 0;
+        int cnt = 0;
 
-        vector<int> prefixSum(n,0);
-        prefixSum[0]= nums[0];
-        for(int i=1;i<n;i++){
-            prefixSum[i]=prefixSum[i-1]+nums[i];
-        }
+        for(int i = 0; i < nums.size(); i++){
+            sum += nums[i];
 
-        unordered_map<int,int> m; //prefixSum , freq 
-        for(int j=0;j<n;j++){
-            if(prefixSum[j]==k) cnt++;
-
-            int val = prefixSum[j] - k; //remove
-            if(m.find(val) != m.end()){
-                cnt +=m[val];
+            if(m.find(sum - k) != m.end()){
+                cnt += m[sum - k];
             }
 
-            if(m.find(prefixSum[j]) == m.end()){
-                m[prefixSum[j] == 0];
-            }
-            m[prefixSum[j]]++;
+            m[sum]++;
         }
+
         return cnt;
     }
 };
