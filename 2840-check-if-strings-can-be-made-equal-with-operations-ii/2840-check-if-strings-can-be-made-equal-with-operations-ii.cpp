e982ac4e49484bd5c754cf9c318e12a8974c627a
sunita <160343306+suma01111@users.noreply.github.com>
2026-03-30 17:58:24 +0530

    Time: 24 ms (40.23%), Space: 20.7 MB (44.25%) - LeetHub

--- 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.cpp
+++ 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.cpp
@@ -1,20 +1,23 @@
+//optimal , by me 
+//IMP is checking even flag then going to odd idex
+
 class Solution {
 public:
     bool checkStrings(string s1, string s2) {
         unordered_map<int,int> m1; //char , freq
         unordered_map<int,int> m2;
 
-        for(int i=0;i<s1.size();i=i+2){
+        for(int i=0;i<s1.size();i=i+2){ //even
             m1[s1[i]]++;
             m2[s2[i]]++;
         }
 
         bool even = false;
-        if(m1==m2) even = true;
+        if(m1==m2) even = true; //IMP
 
 
         if(even){
-            for(int i=1;i<s1.size();i+=2){
+            for(int i=1;i<s1.size();i+=2){ //IMP
                 m1[s1[i]]++;
                 m2[s2[i]]++;
             }
