15f968e259bf6c600bf9d515fdad330954818bf5
sunita <160343306+suma01111@users.noreply.github.com>
2026-04-08 22:09:15 +0530

    Time: 0 ms (100%), Space: 17.5 MB (53.71%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,4 +1,6 @@
 class Solution {
+
+    //lower Bound, then check lb also
 public:
     int lowerBound(vector<int>& nums, int target){
         int l=0, h=nums.size()-1;
@@ -16,7 +18,7 @@ public:
         return lb;
     }
 
-
+    //Upper bound
 public:
     int upperBound(vector<int>& nums, int target){
         int l=0, h=nums.size()-1;
@@ -34,16 +36,18 @@ public:
         return ub;
     }
 
-
+    //main function
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
 
         int LB = lowerBound(nums,target);
-        if(LB==nums.size() || nums[LB]!= target) return {-1,-1};
+        if(LB==nums.size() || nums[LB]!= target) return {-1,-1}; //IMP CHECK
 
-        int UB = upperBound(nums,target) - 1;
+        int UB = upperBound(nums,target) - 1; //IMPP
 
         return {LB,UB};
 
     }
 };
+
+//if tera lb = n-1 (last elemet bhi h ) , no need to check ub cond because it will bw automaticall UB=ub-1 (ex: 5,7,7,8,8,10 ; target = 10)
