ce38788a7844ad6ec6abb0e4e9e28762a5665841
sunita <160343306+suma01111@users.noreply.github.com>
2026-04-09 02:53:37 +0530

    Time: 0 ms (100%), Space: 17.9 MB (58.73%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -8,30 +8,34 @@ public:
 
             if(nums[mid]== target) return true;
 
+            // Handle duplicates: If left, mid, and right are same,
+            // we cannot determine which half is sorted ,shrink search space
             if(nums[left] == nums[mid] && nums[mid]== nums[right]){
                 left++;
                 right--;
-
             }
 
+            // Check if left half is sorted
             else if(nums[left] <= nums[mid]){
-                if(nums[left] <= target && target < nums[mid]){
+                if(nums[left] <= target && target <= nums[mid]){
                     right = mid-1;
                 } else {
                     left = mid +1;
                 }
             }
+
+            //otherwise right half must be sorted
             else {
-                if (nums[mid] < target && target <= nums[right]) {
+                if (nums[mid] <= target && target <= nums[right]) {
                     left = mid + 1;
                 } else {
                     right = mid - 1;
                 }
             }
+
         }
-            return false;
-        
+
+
+        return false;
     }
 };
-
-            
