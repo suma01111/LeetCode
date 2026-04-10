f6bbe58dfda1ae63bd1f98a7ea52ae78fafa9153
sunita <160343306+suma01111@users.noreply.github.com>
2026-04-11 01:11:41 +0530

    Time: 11 ms (8.11%), Space: 30.8 MB (15.8%) - LeetHub

--- 3740-minimum-distance-between-three-equal-elements-i/3740-minimum-distance-between-three-equal-elements-i.cpp
+++ 3740-minimum-distance-between-three-equal-elements-i/3740-minimum-distance-between-three-equal-elements-i.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
     int minimumDistance(vector<int>& nums) {
+        unordered_map<int,vector<int> > m; //IMPP
         int n=nums.size();
-        int minii=INT_MAX;
 
         for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                for(int k=j+1;k<n;k++){
-                    if(nums[i]==nums[j] && nums[j]==nums[k]){
-                        cout<<"x";
-                        int dist = abs(i - j) + abs(j - k) + abs(k - i);
-                        minii=min(dist,minii);
-                    }
+            m[nums[i]].push_back(i); //nums[i], [idx1,idx2,idx3...]
+        }
+
+        int minii = INT_MAX;
+        for(auto it:m){
+            vector<int> &v = it.second; //IMPP
+
+            if(v.size()>=3){
+                for(int i=0;i+2<v.size();i++){
+                    int dist = 2*(v[i+2]-v[i]); //simply formula
+                    minii=min(dist,minii);
                 }
             }
         }
 
-        if(minii==INT_MAX) return -1;
-
-        return minii;
+        return (minii==INT_MAX) ? -1 : minii;
     }
 };
