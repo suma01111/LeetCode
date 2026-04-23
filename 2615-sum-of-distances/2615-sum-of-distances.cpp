107530918a9c8d3c4a794390cf617f195ccbff2c
sunita <160343306+suma01111@users.noreply.github.com>
2026-04-23 13:57:20 +0530

    Time: 68 ms (62.25%), Space: 117.1 MB (48.25%) - LeetHub

---
+++ 2615-sum-of-distances/2615-sum-of-distances.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    vector<long long> distance(vector<int>& nums) {
+        int n = nums.size();
+        unordered_map<int, vector<int>> mp;
+
+        // Step 1: store indices
+        for (int i = 0; i < n; i++) {
+            mp[nums[i]].push_back(i);
+        }
+
+        vector<long long> ans(n, 0);
+
+        // Step 2: process each group
+        for (auto &it : mp) {
+            vector<int> &idx = it.second;
+            int k = idx.size();
+
+            vector<long long> prefix(k, 0);
+            prefix[0] = idx[0];
+
+            for (int i = 1; i < k; i++) {
+                prefix[i] = prefix[i-1] + idx[i];
+            }
+
+            for (int i = 0; i < k; i++) {
+                long long left = 0, right = 0;
+
+                // LEFT
+                if (i > 0) {
+                    left = (long long)i * idx[i] - prefix[i-1];
+                }
+
+                // RIGHT
+                if (i < k-1) {
+                    right = (prefix[k-1] - prefix[i])-(long long)(k-i-1) * idx[i];
+                }
+
+                ans[idx[i]] = left + right;
+            }
+        }
+
+        return ans;
+    }
+};
