eec34db937933af80fc50aa57b6946fc8b4f00d6
sunita <160343306+suma01111@users.noreply.github.com>
2026-05-06 23:18:45 +0530

    Time: 0 ms (100%), Space: 7.7 MB (95.2%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
@@ -1,6 +1,14 @@
+// XOR (^) gives the sum without carry
+// AND (&) followed by left shift (<< 1) gives the carry
+
 class Solution {
 public:
     int getSum(int a, int b) {
-        return a+b;
-    }
+        while(b!=0){
+            int carry = (a&b)<<1;
+        a = a^b;
+        b = carry;
+        }
+    return a;
+    } 
 };
