e6732157bd7c5979cd9eb27ca32b502d6495bf87
sunita <160343306+suma01111@users.noreply.github.com>
2026-05-11 01:38:48 +0530

    Time: 47 ms (11.61%), Space: 57.5 MB (9.39%) - LeetHub

---
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -0,0 +1,41 @@
+// for N vertices I need N-1 edge
+class Solution {
+public:
+    void dfs(int node, vector<vector<int>>& adj, vector<int>& vis){
+        vis[node] =1;
+        for(auto neighbour : adj[node]){
+            if(!vis[neighbour]){
+                dfs(neighbour,adj,vis);
+            }
+        }
+    }
+
+
+public:
+    int makeConnected(int n, vector<vector<int>>& connections) {
+
+        if(connections.size() < n-1) return -1; // minimum edges needed = n-1
+
+        vector<vector<int>> adj(n);
+        //building graph
+        for(auto e: connections){
+            int u=e[0];
+            int v=e[1];
+
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+
+        vector<int> vis(n,0);
+        int comp =0;
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                comp++;
+                dfs(i,adj,vis);
+            }
+        }
+
+
+        return comp-1;
+    }
+};
