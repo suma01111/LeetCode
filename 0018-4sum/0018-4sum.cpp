754a3cb575e5a4892161694c1b52db06e7596134
sunita <160343306+suma01111@users.noreply.github.com>
2026-05-11 15:10:29 +0530

    Time: 27 ms (22.42%), Space: 17.4 MB (52.18%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -12,10 +12,10 @@ public:
         sort(nums.begin(), nums.end()); //sorting 
 
         for(int i = 0; i < n; i++){
-            if(i > 0 && nums[i] == nums[i-1]) continue; //removing duplicates(RP)
+            if(i > 0 && nums[i] == nums[i-1]) continue; //removing duplicates(RD)
 
             for(int j = i+1; j < n; j++){
-                if(j > i+1 && nums[j] == nums[j-1]) continue;//RP
+                if(j > i+1 && nums[j] == nums[j-1]) continue;//RD, Remember j>i+1
 
                 int k = j + 1;
                 int l = n - 1;
@@ -23,13 +23,14 @@ public:
                 while(k < l){
                     long long sum = (long long)nums[i] + nums[j] + nums[k] + nums[l];
 
+                    //DONT FORGET TO MOVE k,l in while loop
                     if(sum == target){
                         ans.push_back({nums[i], nums[j], nums[k], nums[l]});
                         k++;
                         l--;
 
-                        while(k < l && nums[k] == nums[k-1]) k++; //RP
-                        while(k < l && nums[l] == nums[l+1]) l--;//RP
+                        while(k < l && nums[k] == nums[k-1]) k++; //RD
+                        while(k < l && nums[l] == nums[l+1]) l--;//RD
                     }
                     else if(sum < target){
                         k++;
@@ -44,3 +45,6 @@ public:
         return ans;
     }
 };
+
+
+//TC : O(n × n × n) = O(n³) || SC: O(1) (excluding output), O(k) including result storage
