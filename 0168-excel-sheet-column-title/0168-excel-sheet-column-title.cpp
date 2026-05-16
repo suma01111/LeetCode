f61f22ba3be674d21b186ad3a4dec959b95cb0eb
sunita <160343306+suma01111@users.noreply.github.com>
2026-05-16 12:22:15 +0530

    Time: 0 ms (100%), Space: 7.9 MB (23.57%) - LeetHub

--- 0168-excel-sheet-column-title/0168-excel-sheet-column-title.cpp
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.cpp
@@ -15,3 +15,21 @@ public:
         return ans;
     }
 };
+
+/*
+// Excel columns are 1-based:
+            // A = 1, B = 2, ..., Z = 26
+            //
+            // But modulo operation works naturally with 0-based indexing:
+            // A = 0, B = 1, ..., Z = 25
+            //
+            // So before taking modulo,
+            // we reduce colNum by 1 to shift it to 0-based indexing.
+
+    // Convert number to character
+            // 0 -> A
+            // 1 -> B
+            // ...
+            // 25 -> Z
+            ans += (remainder + 'A');
+*/
