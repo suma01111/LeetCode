b72c6a6b6a3172d7bb392ac5469a826eb4d34498
sunita <160343306+suma01111@users.noreply.github.com>
2026-06-19 17:39:55 +0530

    Time: 4 ms (23.17%), Space: 12.7 MB (75.35%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -6,16 +6,18 @@ public:
 
         //finding NG element of nums2 arr
         for(int i=nums2.size()-1;i>=0;i--){
-            while(s.size()>0 && nums2[i]>= s.top()){
+
+            while(s.size()>0 && nums2[i]>= s.top()){ //IMP while loop
                 s.pop();
             }
+
             if(s.empty())      m[nums2[i]] = -1 ;
             else               m[nums2[i]] = s.top();
 
             s.push(nums2[i]);
         }
 
-        //taking reference 
+        //taking reference with help of map
         vector<int> ans;
         for(int i=0;i<nums1.size();i++){
             ans.push_back(m[nums1[i]]);
