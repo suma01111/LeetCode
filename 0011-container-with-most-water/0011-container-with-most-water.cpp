8a81e3663398881120136147de44a0929cd79311
sunita <160343306+suma01111@users.noreply.github.com>
2026-06-24 10:12:35 +0530

    Time: 6 ms (8.02%), Space: 63 MB (47.94%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -4,7 +4,7 @@ public:
         //left and right are index num
         int left =0;
         int right = height.size()-1;
-        int max=0;
+        int maxi=0;
 
         while(left<right){
             int ht_left = height[left];
@@ -15,11 +15,11 @@ public:
             int area = (length * min_ht);
 
             //updating max area
-            max = std::max(max,area);
+            maxi = max(maxi,area);
 
             if(ht_left<ht_right) left++;
             else right--;
         }
-        return max;
+        return maxi;
     }
 };
