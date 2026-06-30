f111836ceec90f02e5c51476f77951ae9a49f797
sunita <160343306+suma01111@users.noreply.github.com>
2026-06-30 13:41:46 +0530

    Time: 8 ms (54.98%), Space: 11 MB (81.8%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -10,11 +10,57 @@ public:
             freq[s[r]-'a']++;
 
             while(freq[0]>=1 &&  freq[1]>=1 && freq[2]>=1){
-                ans += s.size()-r;
-                freq[s[l] - 'a']--;   // IMPORTANT
+                ans += s.size()-r;// IMPORTANT
+                freq[s[l] - 'a']--;   
                 l++;
             }
         }
         return ans;
     }
 };
+
+/*
+Current valid substring:
+
+[0..2] = "abc"
+
+Now if we extend the right end:
+
+[0..3] = "abca"
+[0..4] = "abcab"
+[0..5] = "abcabc"
+
+These are also valid because once a window contains a,b,c, adding more characters cannot remove them.
+*/
+
+/* BY MAP
+class Solution {
+public:
+    int numberOfSubstrings(string s) {
+        int n = s.size();
+        int l = 0, r = 0;
+        long long ans = 0;
+
+        unordered_map<char, int> mp;
+
+        while (r < n) {
+
+            mp[s[r]]++;
+
+            while (mp['a'] > 0 &&
+                   mp['b'] > 0 &&
+                   mp['c'] > 0) {
+
+                ans += (n - r);
+
+                mp[s[l]]--;
+                l++;
+            }
+
+            r++;
+        }
+
+        return ans;
+    }
+};
+*/
