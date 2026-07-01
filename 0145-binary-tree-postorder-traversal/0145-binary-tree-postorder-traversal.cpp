a4c874b2cd651e047f4c30f4ade2ec0cdebeb9d4
sunita <160343306+suma01111@users.noreply.github.com>
2026-07-02 00:04:25 +0530

    Time: 0 ms (100%), Space: 11 MB (44.09%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -1,26 +1,35 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+USING ITERATIVE 2 STACKS
+in Stack1 =>> push node , pop(top node) and push its childer in st1 same
+in Stack2 =>> that pop node from st1 will be pushed here 
+after all traversal , st_1 will be empty and St_2 will be our ans of POSTORDER
  */
 class Solution {
 public:
     vector<int> postorderTraversal(TreeNode* root) {
         vector<int> ans;
-        PostOrder(root,ans);
-        return ans;
-    }
 
-    void PostOrder(TreeNode* node, vector<int>& ans){
-        if (node==NULL) return ;
-        PostOrder(node->left, ans);
-        PostOrder(node->right, ans);
-        ans.push_back(node->val);
+        if(root == nullptr) return ans;
+
+        stack<TreeNode*> st1, st2;
+        st1.push(root);
+
+        while(!st1.empty()) {
+            TreeNode* node = st1.top();
+            st1.pop(); //pop top from st1
+            st2.push(node);//push that node in st2
+
+            //push node children in st1
+            if(node->left) st1.push(node->left);
+            if(node->right) st1.push(node->right);
+        }
+
+        //our final postOrder will be in st2
+        while(!st2.empty()) {
+            ans.push_back(st2.top()->val);
+            st2.pop();
+        }
+
+        return ans;
     }
 };
