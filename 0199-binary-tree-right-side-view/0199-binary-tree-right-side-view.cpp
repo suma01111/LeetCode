692b45ab9c9700014b6a0807246ad6a0acb80b23
sunita <160343306+suma01111@users.noreply.github.com>
2026-07-02 04:36:01 +0530

    Time: 0 ms (100%), Space: 14.8 MB (85.81%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -1,28 +1,21 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+//Reverse =>. Preorder: Root L R)
+//Right → Left -> root
+ //BY dfs
 class Solution {
 public:
     vector<int> rightSideView(TreeNode* root) {
-        vector<int> res;
-        helper(root,0,res);
-        return res;
+        vector<int> output;
+        dfs(root,0,output);//level start with 0
+        return output;
     }
 public:
-    void helper(TreeNode* node, int level, vector<int>& res){
+    void dfs(TreeNode* node, int level, vector<int>& output){
         if(node==NULL) return;
         
-        if(res.size()==level) res.push_back(node->val);//IMP REMEMBER
+        if(output.size()==level) output.push_back(node->val);//IMP REMEMBER
+        //if(output.size() < level), level start from 1, another sol by MIK
 
-        helper(node->right,level+1, res);
-        helper(node->left,level+1, res);
+        dfs(node->right,level+1, output);
+        dfs(node->left,level+1, output);
     }
 };
