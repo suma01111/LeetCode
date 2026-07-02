f0ea20c44c082305b4325cb08cae1041d2b93ec4
sunita <160343306+suma01111@users.noreply.github.com>
2026-07-03 00:35:09 +0530

    Time: 144 ms (85.77%), Space: 189.2 MB (71.8%) - LeetHub

--- 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.cpp
+++ 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.cpp
@@ -1,3 +1,12 @@
+/**
+// Approach:
+// 1. Build a parent map using BFS.
+// 2. Find the starting node while building the map.
+// 3. Perform BFS from the start node.
+// 4. Visit left, right and parent (if not visited).
+// 5. Every BFS level represents one minute.
+// 6. The last processed level gives the total infection time.
+ */
 class Solution {
 public:
     // Step 1: Build parent mapping and find the starting node
@@ -17,7 +26,7 @@ public:
                 startNode = curr;
 
             if (curr->left) {
-                parent[curr->left] = curr;
+                parent[curr->left] = curr;//m[root->left]=root
                 q.push(curr->left);
             }
 
@@ -27,31 +36,30 @@ public:
             }
         }
 
-        return startNode;
+        return startNode; //IMP
     }
+
 
     int amountOfTime(TreeNode* root, int start) {
 
         // Step 1: Build parent map and get start node
         unordered_map<TreeNode*, TreeNode*> parent;
-        TreeNode* startNode = buildParent(root, start, parent);
+        TreeNode* startNode = buildParent(root, start, parent);//IMPP
 
         // Step 2: Start BFS from start node
         queue<TreeNode*> q;
-        unordered_set<TreeNode*> vis;
+        unordered_set<TreeNode*> vis;//REMEMBER
 
-        q.push(startNode);
+        q.push(startNode);//start with startnode in Queue
         vis.insert(startNode);
 
         int time = -1;
 
         while (!q.empty()) {
-
             int size = q.size();
 
             // Process one level (1 minute)
             while (size--) {
-
                 TreeNode* curr = q.front();
                 q.pop();
 
@@ -80,4 +88,4 @@ public:
 
         return time;
     }
-};
+}; 
