836d6c23a14c81a90145325cacb4563ea195657a
sunita <160343306+suma01111@users.noreply.github.com>
2026-07-03 02:10:36 +0530

    Time: 0 ms (100%), Space: 31.3 MB (74.08%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -1,27 +1,55 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ // Approach:
+// 1. Find the leftmost and rightmost heights of the current subtree.
+// 2. If both heights are equal, the subtree is perfect.
+//    Return (2^height - 1).
+// 3. Otherwise, recursively count nodes in left and right subtrees.
+// 4. Answer = 1 + left subtree + right subtree.
  */
+
+ //optimal force in (log2N)
+
 class Solution {
 public:
-    int countNodes(TreeNode* root) {
-         int cnt =0;
-        inOrder(root, cnt);
-        return cnt;
+
+    // Find height by moving to the leftmost node
+    int findLeftHeight(TreeNode* node) {
+        int height = 0;
+
+        while (node) {
+            height++;
+            node = node->left;
+        }
+
+        return height;
     }
 
-    void inOrder(TreeNode* node, int& cnt){
-        if(node == NULL ) return;
+    // Find height by moving to the rightmost node
+    int findRightHeight(TreeNode* node) {
+        int height = 0;
 
-        cnt++;
-        inOrder(node->left,cnt);
-        inOrder(node->right,cnt);
+        while (node) {
+            height++;
+            node = node->right;
+        }
+
+        return height;
+    }
+
+    //MAIN Function
+    int countNodes(TreeNode* root) {
+
+        if (!root) return 0;
+
+        // Step 1: Find leftmost and rightmost heights
+        int lh = findLeftHeight(root);
+        int rh = findRightHeight(root);
+
+        // Step 2: If heights are equal, it's a perfect binary tree
+        if (lh == rh)
+            return (1 << lh) - 1;//(2^height - 1).
+
+        // Step 3: Otherwise recursively count left and right subtree
+        return 1 + countNodes(root->left) + countNodes(root->right);
     }
 };
