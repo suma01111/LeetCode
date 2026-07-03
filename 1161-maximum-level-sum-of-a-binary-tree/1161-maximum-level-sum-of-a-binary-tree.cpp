c846cc70f4422ddc3600ec815fc47d8099d847d3
sunita <160343306+suma01111@users.noreply.github.com>
2026-07-03 23:34:26 +0530

    Time: 0 ms (100%), Space: 109.6 MB (31.74%) - LeetHub

--- 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.cpp
+++ 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.cpp
@@ -16,12 +16,12 @@ public:
             for (int i = 0; i < size; i++) {
                 TreeNode* node = q.front();
                 q.pop();
-                sumLevel += node->val;
+                sumLevel += node->val; //IMP
                 if (node->left != NULL) q.push(node->left);
                 if (node->right != NULL) q.push(node->right);
             } //since we pushed L and R, again loop start from i=0 
 
-            if (maxSum < sumLevel) {
+            if (maxSum < sumLevel) { //update after every level
                 maxSum = sumLevel;
                 ans = level;  
             }
