621137484c7ed8c5a2a36fb545fd9ab2fc58f166
sunita <160343306+suma01111@users.noreply.github.com>
2026-07-05 00:57:07 +0530

    Time: 0 ms (100%), Space: 7.9 MB (23.86%) - LeetHub

---
+++ 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isSameAfterReversals(int num) {
+        return getReverse(num);
+    }
+
+    bool getReverse(int n){
+        int reverse=0;
+        string original= to_string(n);
+
+        while(n>0){
+            int dig=n%10;
+            reverse= reverse*10 + dig;
+            n=n/10;
+        }
+        
+        string r = to_string(reverse);
+
+        return (original.size() == r.size());
+    }
+};
