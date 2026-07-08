1127ffddafa3f087aff8d7bc3bc7b0e5160f9b70
sunita <160343306+suma01111@users.noreply.github.com>
2026-07-09 00:46:28 +0530

    Time: 3 ms (75.47%), Space: 20.3 MB (17.51%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,12 +1,17 @@
+//by cycle detection in directed graph approach
+// Approach:
+// Build adjacency list (as edge is given)
+// Detect cycle using DFS + vis[] + pathVis[].
+// If cycle exists -> cannot finish all courses.
+
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-
         vector<vector<int>> adj(numCourses);
 
-        for(auto it : prerequisites){
-            int u = it[0];
-            int v = it[1];
+        for(auto it: prerequisites){
+            int u=it[0];
+            int v=it[1];
             adj[v].push_back(u);
         }
 
@@ -24,23 +29,22 @@ public:
 
 
     bool dfs(int node, vector<int>& vis, vector<int>& pathVis, vector<vector<int>>& adj){
-
         vis[node]=1;
-        pathVis[node]=1;
+        pathVis[node]=1;  // Mark node in current DFS path
 
         for(auto it : adj[node]){
-
             if(!vis[it]){
-                if(!dfs(it, vis, pathVis, adj))
+                if(!dfs(it,vis,pathVis,adj)) // If a cycle is found in any recursive DFS call, immediately return it to the caller.
                     return false;
             }
-
-            else if(pathVis[it]){
+            else if(pathVis[it]){ // Back edge found -> cycle exists
                 return false;
             }
         }
 
+        // Backtracking: remove node from current DFS path
         pathVis[node] = 0;
-        return true;
+
+        return true;    // No cycle found from this node
     }
 };
