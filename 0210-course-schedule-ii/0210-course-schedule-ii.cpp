13c9a7f3349a64a6f22d4a52bfa0d708aa069321
sunita <160343306+suma01111@users.noreply.github.com>
2026-07-09 04:05:36 +0530

    Time: 0 ms (100%), Space: 18.2 MB (70.94%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,51 +1,53 @@
-//by cycle detection in directed graph approach
-//topo vector added for topological sort (store value while backtracking and then reverse order will give topo order)
+/*Approach 2 
+    Kahn's Algorithm (BFS)
+    - Compute indegree of every node.
+    - Push all nodes with indegree 0 into queue.
+    - Process queue:
+         * Add node to answer.
+         * Reduce indegree of neighbours.
+         * Push neighbour if indegree becomes 0.
+    - If topo size == total nodes -> return topo.
+    - Else cycle exists -> return {}.*/
 
 class Solution {
-    bool dfs(int node, vector<vector<int>> &adj,
-             vector<int> &vis, vector<int> &pathVis,
-             vector<int> &topo){
-
-        vis[node]=1;
-        pathVis[node]=1;
-
-        for(int nei: adj[node]){
-            if(!vis[nei]){
-                if(dfs(nei,adj,vis,pathVis,topo))
-                    return true;
-            }
-            else if(pathVis[nei])
-                return true;
-        }
-        // Remove node from current DFS path
-        pathVis[node]=0;
-        // Store node after visiting all its neighbours
-        topo.push_back(node);
-        return false;
-    }
-
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        // Build adjacency list and indegree array
         vector<vector<int>> adj(numCourses);
-        for(auto &it: prerequisites){
-            adj[it[1]].push_back(it[0]);
+        vector<int> indegree(numCourses, 0);
+
+        for (auto &it : prerequisites) {
+            adj[it[1]].push_back(it[0]); // prerequisite -> course
+            indegree[it[0]]++;
+        }
+        
+        queue<int> q;// Push all nodes having indegree 0
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0)
+                q.push(i);
         }
 
-        vector<int> vis(numCourses,0);
-        vector<int> pathVis(numCourses,0);
         vector<int> topo;
+        // BFS (Kahn's Algorithm)
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
 
-        for(int i=0;i<numCourses;i++){
-            if(!vis[i]){
-                if(dfs(i,adj,vis,pathVis,topo))
-                    return {};
+            topo.push_back(node);// Current course can be taken now
+
+            for (int nei : adj[node]) {// Remove all outgoing edges
+                indegree[nei]--;
+
+                if (indegree[nei] == 0)
+                    q.push(nei);
             }
         }
-        reverse(topo.begin(),topo.end()); //just reverse array, as start pushed course can come before any course so put at last
-        return topo;
+
+        // If all courses are processed, return topo order
+        // Otherwise a cycle exists, so it's impossible
+        if (topo.size() == numCourses)
+            return topo;
+
+        return {};
     }
 };
-
-
-// The important point is:
-// A node is added to ans only after all its neighbours (dependent courses) have been processed.
