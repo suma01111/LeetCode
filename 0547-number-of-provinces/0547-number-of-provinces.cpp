d3a4b9822af9a49b7e0f8f712dd43529ed7c1789
sunita <160343306+suma01111@users.noreply.github.com>
2026-07-12 00:31:31 +0530

    Time: 0 ms (100%), Space: 19.5 MB (61.49%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,29 +1,64 @@
-//Approach -1, dfs
-//without converting into adj list, direct access to adj matrix
+// Approach 2: BY DSU (disjoint set union)
+
 class Solution {
+public:
+    vector<int> parent, size;
+    // Find the ultimate parent (with Path Compression)
+    int findParent(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findParent(parent[node]);
+    }
 
-    void dfs(int node,vector<vector<int>>& isConnected,vector<bool>& vis) {
-        vis[node]=true;
+    // Merge two components using Union by Size
+    void unionBySize(int u, int v) {
+        u = findParent(u);
+        v = findParent(v);
 
-        for (int nei=0;nei<isConnected.size();nei++) {
-    //Because every column represents a possible neighbor, trversing col of node
-            if (isConnected[node][nei]==1 && !vis[nei]) { //IMPPP, new
-                dfs(nei,isConnected,vis);
-            }
+        if (u == v) return;
+
+        if (size[u] < size[v]) {
+            parent[u] = v;
+            size[v] += size[u];
+        }
+        else {
+            parent[v] = u;
+            size[u] += size[v];
         }
     }
 
-public:
-    int findCircleNum(vector<vector<int>>& isConnected) {
-        int n=isConnected.size();
-        vector<bool> vis(n,false);
-        int provinces=0;
+    // Approach:
+    // 1. Initially, every city is its own province.
+    // 2. Traverse the adjacency matrix.
+    // 3. If city i and j are directly connected, merge their sets using DSU.
+    // 4. After all unions, count how many nodes are their own parent.
+    // 5. That count is the number of provinces.
 
-        for (int i=0;i<n;i++) {
-            if (!vis[i]) {
-                dfs(i,isConnected,vis);
-                provinces++;
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+
+        parent.resize(n);
+        size.assign(n, 1);
+
+        for (int i = 0; i < n; i++) parent[i] = i;
+
+        // Traverse only the upper triangle to avoid duplicate edges
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+
+                // If two cities are directly connected, merge them
+                if (isConnected[i][j] == 1) {
+                    unionBySize(i, j);
+                }
             }
+        }
+
+        int provinces = 0;
+
+        // Every ultimate parent represents one province
+        for (int i = 0; i < n; i++) {
+            if (findParent(i)==i)
+                provinces++;
         }
 
         return provinces;
